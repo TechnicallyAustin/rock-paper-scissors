7ff1397a81435ccc9928e069cb9ba8d306ddc1a5
TechnicallyAustin <TechnicallyAustin@gmail.com>
2022-12-02 23:35:36 -0800

    added html js index files and linked them bult base methdos for RPS game

---
+++ index.js
@@ -0,0 +1,18 @@
+const CHOICES = ["rock", "paper", "scissors"]
+userInpute = prompt()
+function getComputerChoice() {
+// randomly returns an element of the choices array
+// case Insensitive rock or ROCK or RoCk
+}
+
+function play(playerSelection, computerSelection) {
+    // plays a single roung of rock paper scissors
+    //return results
+}
+
+function game() {
+    // call play() function at least 5 times to simulate a game
+}
+
+
+
