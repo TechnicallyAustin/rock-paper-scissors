8fcb3420eb99ebd41679502098148b0b13b47720
TechnicallyAustin <TechnicallyAustin@gmail.com>
2022-12-02 23:51:10 -0800

    building out method logic

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-const CHOICES = ["rock", "paper", "scissors"]
-userInpute = prompt()
+let choices = ["rock", "paper", "scissors"]
 function getComputerChoice() {
 // randomly returns an element of the choices array
 // case Insensitive rock or ROCK or RoCk
+let choices = ["rock", "paper", "scissors"];
+let cpuChoice = Math.floor(Math.random() * 2);
+
 }
 
 function play(playerSelection, computerSelection) {
     // plays a single roung of rock paper scissors
     //return results
+
 }
 
 function game() {
     // call play() function at least 5 times to simulate a game
+    for (let i = 0; i < 5; i++){
+        play(promptUser(),)
+    }
+}
+
+function promptUser() {
+let userInput = prompt("Please enter Rock Paper or Scissors", "Rock").toLowerCase()
+return userInput 
+
 }
 
 
