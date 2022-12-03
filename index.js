7add9f3e05a07ddbca6bd90c5fc4cbf8658457e2
TechnicallyAustin <TechnicallyAustin@gmail.com>
2022-12-02 23:59:24 -0800

    adding switch case for play

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
-let choices = ["rock", "paper", "scissors"]
+
 function getComputerChoice() {
-// randomly returns an element of the choices array
-// case Insensitive rock or ROCK or RoCk
-let choices = ["rock", "paper", "scissors"];
-let cpuChoice = Math.floor(Math.random() * 2);
+    let choices = ["rock", "paper", "scissors"];
+    let cpuChoice = Math.floor(Math.random() * 2);
+    return choices[cpuChoice]
 
 }
 
 function play(playerSelection, computerSelection) {
-    // plays a single roung of rock paper scissors
+    // plays a single round of rock paper scissors
     //return results
+    switch(playerSelection) {
+        case rock:
+            if (computerSelection === "scissors") {
+                console.log(`you picked: rock -- the computer picked ${computerSelection}`)
+                console.log
+            }
+        break
+
+        case papper:
+
+        break
+
+        case scissors:
+
+        break
+
+        default:
+    }
+
 
 }
 
 function game() {
     // call play() function at least 5 times to simulate a game
     for (let i = 0; i < 5; i++){
-        play(promptUser(),)
+        play(promptUser(), getComputerChoice)
     }
 }
 
