2eae2a4f75ede5bba6e69558babf95c779b3bb49
TechnicallyAustin <TechnicallyAustin@gmail.com>
2022-12-03 13:27:29 -0800

    added win tracking

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let userWins;
-let cpuWins;
-let winArray = [];
+
+let userWins = 0;
+let cpuWins = 0;
 
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
@@ -10,25 +10,32 @@ function getComputerChoice() {
 }
 
 function play(playerSelection, computerSelection) {
+
     // plays a single round of rock paper scissors
     //return results
     if (playerSelection === computerSelection) {
         console.log("Draw");
     }
     else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
-        console.log(`you win ${playerSelection}`);
+        userWins += 1
+        console.log(`you win ${playerSelection}`, userWins)
     } else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
-        console.log(`you win ${playerSelection}`);
+        userWins += 1
+        console.log(`you win ${playerSelection}`, userWins);
     } else if ((playerSelection === "paper") && (computerSelection === "rock")){
-        console.log(`you win ${playerSelection}`)
+        userWins += 1
+        console.log(`you win ${playerSelection}`, userWins)
     } else {
-        console.log("you lose")
+        cpuWins += 1
+        console.log("you lose", cpuWins)
     }
 
 
 }
 
 function game() {
+    userWins = 0;
+    cpuWins = 0;
     // call play() function at least 5 times to simulate a game
     for (let i = 0; i < 5; i++){
         play(promptUser(), getComputerChoice)
