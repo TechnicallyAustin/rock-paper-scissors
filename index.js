f7ae13753e0adff0e83cae67b8d2a7e5f72dc411
TechnicallyAustin <TechnicallyAustin@gmail.com>
2022-12-04 02:40:42 -0800

    formatting

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
-
 let userWins = 0;
 let cpuWins = 0;
 
-
-
 function getComputerChoice() {
-    let choices = ["rock", "paper", "scissors"];
-    let cpuChoice = choices[Math.floor(Math.random() * 3)];
-    return cpuChoice
+  let choices = ["rock", "paper", "scissors"];
+  let cpuChoice = choices[Math.floor(Math.random() * 3)];
+  return cpuChoice;
 }
 
-
 function play(playerSelection, computerSelection) {
-let win =
-  "you win using " +
-  `${playerSelection} ` +
-  "the cpu picked " +
-  `${computerSelection} ` +
-  "Score -- " +
-  "User: " +
-  `${userWins + 1} ` +
-  "Cpu: " +
-  `${cpuWins} `;
+  let win =
+    "you win using " +
+    `${playerSelection} ` +
+    "the cpu picked " +
+    `${computerSelection} ` +
+    "Score -- " +
+    "User: " +
+    `${userWins + 1} ` +
+    "Cpu: " +
+    `${cpuWins} `;
 
-let lose =
-  "you lose using " +
-  `${playerSelection} ` +
-  "the cpu picked " +
-  `${computerSelection} `+
-  "Score -- " +
-  "User: " +
-  `${userWins} ` +
-  "Cpu: " +
-  `${cpuWins + 1} `;
+  let lose =
+    "you lose using " +
+    `${playerSelection} ` +
+    "the cpu picked " +
+    `${computerSelection} ` +
+    "Score -- " +
+    "User: " +
+    `${userWins} ` +
+    "Cpu: " +
+    `${cpuWins + 1} `;
 
   let draw =
     "It's a Draw! " +
@@ -46,40 +42,35 @@ let lose =
     "Cpu: " +
     `${cpuWins + 1} `;
 
-
   // plays a single round of rock paper scissors
-    //return results
-    if (playerSelection === computerSelection) {
-        alert(draw);
-    }
-    else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
-        userWins += 1;
-        alert(win);
-    
-    } else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
-        userWins += 1;
-        alert(win)
-    
-    } else if ((playerSelection === "paper") && (computerSelection === "rock")){
-        userWins += 1;
-        alert(win);
-    
-    } else {
-        cpuWins += 1
-        alert(lose);
-    }
+  //return results
+  if (playerSelection === computerSelection) {
+    alert(draw);
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    userWins += 1;
+    alert(win);
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    userWins += 1;
+    alert(win);
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    userWins += 1;
+    alert(win);
+  } else {
+    cpuWins += 1;
+    alert(lose);
+  }
 }
 
 function game() {
-    let totalWins = cpuWins + userWins;
-    // call play() function at least 5 times to simulate a game
-    for (let i = 0; i < 5; i++){
-        play(promptUser(), getComputerChoice())
-        if ((totalWins === 5) && (userWins > cpuWins)){
-            alert("You Win the Game!")
-        }
-        alert("You Lose the Game!")
+  let totalWins = cpuWins + userWins;
+  // call play() function at least 5 times to simulate a game
+  for (let i = 0; i < 5; i++) {
+    play(promptUser(), getComputerChoice());
+    if (totalWins === 5 && userWins > cpuWins) {
+      alert("You Win the Game!");
     }
+    alert("You Lose the Game!");
+  }
 }
 
 function winner() {
@@ -90,10 +81,8 @@ function winner() {
 }
 
 function promptUser() {
-let userInput = prompt("Please enter Rock Paper or Scissors", "Rock").toString().toLowerCase()
-return userInput 
-
+  let userInput = prompt("Please enter Rock Paper or Scissors", "Rock")
+    .toString()
+    .toLowerCase();
+  return userInput;
 }
-
-
-
