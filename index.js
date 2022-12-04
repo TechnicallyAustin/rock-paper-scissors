ecf7d89cd9883437fdb17f089ed2f1bb33221973
TechnicallyAustin <TechnicallyAustin@gmail.com>
2022-12-04 02:29:05 -0800

    restructuring variables as functions for win lose and draw

--- index.js
+++ index.js
@@ -3,35 +3,54 @@ let userWins = 0;
 let cpuWins = 0;
 
 
+
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
     let cpuChoice = choices[Math.floor(Math.random() * 3)];
     return cpuChoice
+}
 
+function roundWin() {
+    let win =
+      "you win using " +
+      `${playerSelection} ` +
+      "the cpu picked " +
+      `${computerSelection} ` +
+      "Score -- " +
+      "User: " +
+      `${userWins + 1} ` +
+      "Cpu: " +
+      `${cpuWins} `;
+}
+function roundloss() {
+    let lose =
+      "you lose using " +
+      `${playerSelection} ` +
+      "the cpu picked " +
+      `${computerSelection} ` +
+      "Score -- " +
+      "User: " +
+      `${userWins} ` +
+      "Cpu: " +
+      `${cpuWins + 1} `;
+}
+
+function draw() {
+    let draw =
+      "It's a Draw! " +
+      "you used " +
+      `${playerSelection} ` +
+      "the cpu picked " +
+      `${computerSelection} ` +
+      "Score -- " +
+      "User: " +
+      `${userWins} ` +
+      "Cpu: " +
+      `${cpuWins + 1} `;
 }
 
 function play(playerSelection, computerSelection) {
-let win =
-  "you win using " +
-  `${playerSelection} ` +
-  "the cpu picked " +
-  `${computerSelection} ` +
-  "Score -- " +
-  "User: " +
-  `${userWins + 1} ` +
-  "Cpu: " +
-  `${cpuWins} `;
 
-let lose =
-  "you lose using " +
-  `${playerSelection} ` +
-  "the cpu picked " +
-  `${computerSelection} `+
-  "Score -- " +
-  "User: " +
-  `${userWins} ` +
-  "Cpu: " +
-  `${cpuWins + 1} `;
 
   let draw =
     "It's a Draw! " +
@@ -53,12 +72,10 @@ let lose =
     }
     else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
         userWins += 1;
-       // console.log(win);
         alert(win);
     
     } else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
         userWins += 1;
-      //  console.log(win);
         alert(win)
     
     } else if ((playerSelection === "paper") && (computerSelection === "rock")){
@@ -67,16 +84,23 @@ let lose =
     
     } else {
         cpuWins += 1
-      //  console.log(lose);
         alert(lose);
     }
 }
 
 function game() {
+    let totalWins = cpuWins + userWins;
     // call play() function at least 5 times to simulate a game
     for (let i = 0; i < 5; i++){
         play(promptUser(), getComputerChoice())
     }
+}
+
+function winner() {
+  // add a you win or you lost message
+  // evaluate cpuWins vs userWins
+  // if user wins are greater output a "You won the game method"
+  // otherwise output "you lost the game"
 }
 
 function promptUser() {
