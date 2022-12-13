43870e54f6fc534fbbe08ebb487412723d50aa7e
TechnicallyAustin <TechnicallyAustin@gmail.com>
2022-12-13 13:21:58 -0700

    structuring design and adding logic

--- rps.js
+++ rps.js
@@ -1,3 +1,6 @@
+let userScore = 0;
+let cpuScore = 0;
+
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
     let cpuChoice = choices[Math.floor(Math.random() * 3)];
@@ -7,9 +10,16 @@ function getComputerChoice() {
 function userInput() {}
 
 function play(userChoice, cpuChoice) {
+    let results =;
+    let userScore=;
+    let cpuScore =;
+    let roundsPlayed=;
+    let winner =;
     switch (userChoice) {
         case "rock":
             if (cpuChoice === "rock") {
+
+
             }
             
             // use Jquery to update the DOM with
@@ -37,17 +47,28 @@ function play(userChoice, cpuChoice) {
 
 function createButtons() {
     let gameDiv = document.getElementById("game-div")
-    let rockButton = gameDiv.appendChild(document.createElement("button"));
-    rockButton.textContent = "Rock"
+    let rockDiv = document.getElementById("rock-div") ;
+    let paperDiv = document.getElementById("paper-div") ;
+    let scissorDiv = document.getElementById("scissor-div") ;
+
+    let rockButton = rockDiv.appendChild(document.createElement("button"));
     rockButton.setAttribute('id', 'rock-button')
+    let rockButtonImg = rockButton.appendChild(document.createElement('img'));
+    rockButtonImg.setAttribute("src", "assests/rock.jpg");
+    rockButtonImg.setAttribute('id', 'rock-button-image')
 
-    let paperButton = gameDiv.appendChild(document.createElement("button"));
-    paperButton.textContent = "Paper"
+    let paperButton = paperDiv.appendChild(document.createElement("button"));
     paperButton.setAttribute('id', 'paper-button')
+    let paperButtonImg = paperButton.appendChild(document.createElement("img"));
+    paperButtonImg.setAttribute("src", "assests/paper.jpg");
+    paperButtonImg.setAttribute("id", "paper-button-image");
 
-    let scissorsButton = gameDiv.appendChild(document.createElement("button"));
-    scissorsButton.textContent = "Scissors"
-    scissorsButton.setAttribute('id', 'scissor-button')
+    let scissorButton = scissorDiv.appendChild(document.createElement("button"));
+    scissorButton.setAttribute('id', 'scissor-button')
+    let scissorButtonImg = scissorButton.appendChild(document.createElement("img"));
+    scissorButtonImg.setAttribute("src", "assests/scissors.jpg");
+    scissorButtonImg.setAttribute("id", "paper-button-image");
+
 }
 
 function addButtonEventListeners() {
