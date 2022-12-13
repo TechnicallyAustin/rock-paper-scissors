cc98a170750b4a07c3a5d7072a99f493ec4ba3b0
TechnicallyAustin <TechnicallyAustin@gmail.com>
2022-12-13 13:44:32 -0700

    working through logic and displaying results

--- rps.js
+++ rps.js
@@ -10,15 +10,35 @@ function getComputerChoice() {
 function userInput() {}
 
 function play(userChoice, cpuChoice) {
-    let results =;
-    let userScore=;
-    let cpuScore =;
-    let roundsPlayed=;
-    let winner =;
+    let results = document.getElementById('match-value-text');
+    let userScore = document.getElementById('user-value-text');
+    let cpuScore = document.getElementById('cpu-value-text');
+    let roundsPlayed = document.getElementById('round-value-text');
+    let winner = document.getElementById('winner-value-text');
+
+
+    
+ 
     switch (userChoice) {
         case "rock":
             if (cpuChoice === "rock") {
-
+                results.textContent = "Draw"
+                userScore.textContent = "Number of user wins"
+                cpuScore.textContent = "Number of CPU wins"
+                roundsPlayed.textContent = "Total Number of Rounds Played"
+                winner.textContent = "Winner is..."
+            } else if (cpuChoice === "paper") {
+                   results.textContent = "Loss";
+                   userScore.textContent = "Number of user wins";
+                   cpuScore.textContent = "Number of CPU wins";
+                   roundsPlayed.textContent = "Total Number of Rounds Played";
+                   winner.textContent = "Winner is...";
+            } else if (cpuChoice === "scissors") {
+                   results.textContent = "Win";
+                   userScore.textContent = "Number of user wins";
+                   cpuScore.textContent = "Number of CPU wins";
+                   roundsPlayed.textContent = "Total Number of Rounds Played";
+                   winner.textContent = "Winner is...";
 
             }
             
