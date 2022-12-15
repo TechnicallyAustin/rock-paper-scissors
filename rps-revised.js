173b8b2a9ddc9b20c8d3c86425350c1bc36cad63
TechnicallyAustin <TechnicallyAustin@gmail.com>
2022-12-15 11:03:29 -0700

    optimizing logic

---
+++ rps-revised.js
@@ -0,0 +1,78 @@
+function createButtons() {
+    // calls on button creation methods
+    createRockButton()
+    createPaperButton()
+    createScissorsButton()
+}
+
+function createRockButton() {
+    // creates a Rock button
+}
+
+function createPaperButton() {
+    //creates a Paper button
+}
+
+function createScissorsButton() {
+    //creates a Scissor button
+}
+
+function createButtonEventListner() {
+    // calls on Button event listener methods
+    rockEventListener()
+    PaperEventListener()
+    scissorsEventListener()
+}
+
+function rockEventListener() {
+    // creates a rock evenet listener
+}
+
+function PaperEventListener() {
+    // createse a paper event listener
+}
+
+function scissorsEventListener() {
+    // creates a scissors event listener
+}
+
+function addButtonAttributes() {
+    // calls on add attributes methods
+    rockAttributes()
+    paperAttributes()
+    scissorsAttributes()
+}
+
+function rockAttributes() {
+    // adds id, src, alt
+}
+
+function paperAttributes() {
+  // adds id, src, alt
+}
+
+function scissorsAttributes() {
+  // adds id, src, alt
+}
+
+function getComputerChoice() {
+// generates the computeres selection
+}
+
+function getUserChoice() {
+    // usereInput is equal to whichevere Event is triggered
+}
+
+function playRound(userChoice, computerChoice) {
+    // plays a single roung of RPS
+}
+
+function winner() {
+    // after n rounds comparee user wins to computer wins
+}
+
+function gameLength() {
+    // controls how many rounds the game will run for
+}
+
+
