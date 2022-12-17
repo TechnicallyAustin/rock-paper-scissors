8d191f5a0cfc55fc864f082cb9521d023dc23ec1
TechnicallyAustin <TechnicallyAustin@gmail.com>
2022-12-16 19:59:44 -0700

    working on event listeners

--- rps-revised.js
+++ rps-revised.js
@@ -1,135 +1,142 @@
 function game() {
- let userScore = 0;
- let cpuScore = 0;
- let rounds = 0;
+  let userScore = 0;
+  let cpuScore = 0;
+  let rounds = 0;
 
-    const playGame = () => {
+  const createButtons = () => {
+    const gameDiv = document.getElementById("game-div");
+    const rockButtonSelector = document.getElementById("rock-button");
+    const paperButtonSelector = document.getElementById("paper-button");
+    const scissorsButtonSelector = document.getElementById("scissor-button");
+    const playerChoices = [
+      rockButtonSelector,
+      paperButtonSelector,
+      scissorsButtonSelector,
+    ];
+    const roundsPlayed = document.getElementById("round-value-text");
+    const cpuChoices = ["rock", "paper", "scissors"];
+    const element = Math.floor(Math.random() * 3);
+    const cpuChoice = cpuChoices[element];
+    let roundsPlayedText = document.getElementById("round-value-text");
 
+    const createRockButton = () => {
+      const rockButton = gameDiv.appendChild(document.createElement("button"));
+      rockButton.setAttribute("id", "rock-button");
+      const rockImage = rockButton.appendChild(document.createElement("img"));
+      rockImage.setAttribute("id", "rock-button-image");
+      rockImage.setAttribute("src", "assets/rock.jpg");
+      rockImage.setAttribute("alt", "image of a rock");
+    };
+    createRockButton();
 
-        const createButtons = () => {
-        const gameDiv = document.getElementById("game-div");
-          const newRockButton = gameDiv.appendChild(document.createElement("button"));
-          const newPaperButton = gameDiv.appendChild(document.createElement("button"));
-          const newScissorButton = gameDiv.appendChild(document.createElement("button"));
-          const rockButtonSelector = document.getElementById("rock-button");
-          const paperButtonSelector = document.getElementById("paper-butotn");
-          const scissorsButtonSelector = document.getElementById("scissors-button");
-          const playerChoices = [newRockButton, newPaperButton, newScissorButton];
-          const roundsPlayed = document.getElementById("round-value-text");
-          const cpuChoices = ["rock", "paper", "scissors"];
-          const element = Math.floor(Math.random() * 3);
-          const cpuChoice = cpuChoices[element];
+    const createPaperButton = () => {
+      const paperButton = gameDiv.appendChild(document.createElement("button"));
+      paperButton.setAttribute("id", "paper-button");
+      const paperImage = paperButton.appendChild(document.createElement("img"));
+      paperImage.setAttribute("id", "paper-button-image");
+      paperImage.setAttribute("src", "assets/paper.jpg");
+      paperImage.setAttribute("alt", "image of paper");
+    };
+    createPaperButton();
 
+    const createScissorsButton = () => {
+      const scissorButton = gameDiv.appendChild(
+        document.createElement("button")
+      );
+      scissorButton.setAttribute("id", "scissor-button");
+      const scissorImage = scissorButton.appendChild(
+        document.createElement("img")
+      );
+      scissorImage.setAttribute("id", "scissor-button-image");
+      scissorImage.setAttribute("src", "assets/scissors.jpg");
+      scissorImage.setAttribute("alt", "image of scissors");
+    };
+    createScissorsButton();
 
-            const createRockButton = () => {
-              const rockButton = gameDiv.appendChild(document.createElement("button"));
-              rockButton.setAttribute("id", "rock-button");
-              rockButton.setAttribute("src", "assets/rock.jpg");
-              rockButton.setAttribute("alt", "image of a rock");
-            }
-
-            const createPaperButton = () => {
-              const paperButton = gameDiv.appendChild(document.createElement("button"));
-              paperButton.setAttribute("id", "paper-button");
-              paperButton.setAttribute("src", "assets/paper.jpg");
-              paperButton.setAttribute("alt", "image of paper");
-            }
-
-            const createScissorsButton = () => {
-              const scissorButton = gameDiv.appendChild(document.createElement("button"));
-              scissorButton.setAttribute("id", "scissor-button");
-              scissorButton.setAttribute("src", "assets/scissors.jpg");
-              scissorButton.setAttribute("alt", "image of scissors");
-            }
-
-            playerChoices.forEach(choice => {
-                choice.addEventListener('click', () => {
-                    console.log(rounds)
-                    rounds++
-                    winner(choice, cpuChoice)
-                        if (rounds === 10) {
-                            endOfGame(playerChoices, roundsPlayed)
-                        }
-                });
-            });
+    playerChoices.forEach((choice) => {
+      choice.addEventListener("click", () => {
+        console.log(rounds, choice, cpuChoice);
+        rounds++;
+        winner(choice, cpuChoice);
+        roundsPlayedText.textContent = rounds;
+        if (rounds === 10) {
+          endOfGame(playerChoices, roundsPlayed);
         }
-        createButtons()
-    
+      });
+    });
+  };
+  createButtons();
 
-        const winner = (userChoice, cpuChoice) => {
-        let results = document.getElementById("match-value-text");
-         let userScoreText = document.getElementById("user-value-text");
-         let cpuScoreText = document.getElementById("cpu-value-text");
-         let selection = document.getElementById("selection-text");
+  const playGame = () => {
+    const winner = (userChoice, cpuChoice) => {
+      let results = document.getElementById("match-value-text");
+      let userScoreText = document.getElementById("user-value-text");
+      let cpuScoreText = document.getElementById("cpu-value-text");
+      let selection = document.getElementById("selection-text");
 
-         if (cpuChoice === userChoice){
-            selection.textContent = `It's a Draw! Computer Picked ${cpuChoice}`
-            results.textContent = 'Draw'
+      if (cpuChoice === userChoice) {
+        selection.textContent = `It's a Draw! Computer Picked ${cpuChoice}`;
+        results.textContent = "Draw";
+        userScoreText.textContent = `${userScore}`;
+        cpuScoreText.textContent = `${cpuScore}`;
+      } else if (cpuChoice === "rock") {
+        if (userChoice === "paper") {
+          selection.textContent = `You Win! Computer Picked ${cpuChoice}`;
+          userScore++;
+          userScoreText.textContent = `${userScore}`;
+          cpuScoreText.textContent = `${cpuScore}`;
+          results.textContent = "Win";
+        } else {
+          selection.textContent = `You Lose! Computer Picked ${cpuChoice}`;
+          results.textContent = "Lose";
+          cpuScore++;
+          cpuScoreText.textContent = `${cpuScore}`;
+          userScoreText.textContent = `${userScore}`;
+          results.textContent = "Lose";
         }
-
-             else if (cpuChoice === 'rock') {
-                if (userChoice === 'paper') {
-                    selection.textContent = `You Win! Computer Picked ${cpuChoice}`
-                    userScore++
-                    userScoreText.textContent = `${userScore}`
-                    results.textContent = 'Win'}
-                    else {
-                     selection.textContent = `You Lose! Computer Picked ${cpuChoice}`
-                     results.textContent = 'Lose'
-                     cpuScore++
-                     cpuScoreText.textContent = `${cpuScore}`
-                    }
-            }
-
-            else if (cpuChoice === 'paper') {
-                if (userChoice === 'scissors'){
-                    selection.textContent = `You Win! Computer Picked ${cpuChoice}`
-                    userScore++
-                    userScoreText.textContent = `${userScore}`
-                } else {
-                    selection.textContent = `You Lose! Computer Picked ${cpuChoice}`
-                    cpuScore++
-                    cpuScoreText.textContent = `${cpuScoreText}`
-                }
-            } else if (cpuChoice === 'scissors'){
-                if (userChoice === 'rock'){
-                    selection.textContent = `You Win! Computer Picked ${cpuChoice}`
-                    userScore++
-                    userScoreText.textContent = `${userScore}`
-                }
-            } else if (userChoice === 'paper'){
-                selection.textContent = `You Lose! Computer Picked ${cpuChoice}`
-                cpuScore++
-                cpuScoreText.textContent = `${cpuChoice}`
-            }
-    }
-
-        const endOfGame = (userChoices, round) => {
-            let winner = document.getElementById("winner-value-text");
-                if (userScore === cpuScore){
-                    winner.textContent = "It's a Tie!"
-                } else if (userScore > cpuScore){
-                    winner.textContent = 'You Win the Game!'
-                } else {
-                    winner.textContent = 'You Lose!'
-                }
+      } else if (cpuChoice === "paper") {
+        if (userChoice === "scissors") {
+          selection.textContent = `You Win! Computer Picked ${cpuChoice}`;
+          userScore++;
+          userScoreText.textContent = `${userScore}`;
+          results.textContent = "Win";
+        } else {
+          selection.textContent = `You Lose! Computer Picked ${cpuChoice}`;
+          cpuScore++;
+          cpuScoreText.textContent = `${cpuScore}`;
+          userScoreText.textContent = `${userScore}`;
+          results.textContent = "Lose";
         }
-    }
-    playGame()
-    
+      } else if (cpuChoice === "scissors") {
+        if (userChoice === "rock") {
+          selection.textContent = `You Win! Computer Picked ${cpuChoice}`;
+          userScore++;
+          userScoreText.textContent = `${userScore}`;
+          results.textContent = "Win";
+        }
+      } else if (userChoice === "paper") {
+        selection.textContent = `You Lose! Computer Picked ${cpuChoice}`;
+        cpuScore++;
+        cpuScoreText.textContent = `${cpuScore}`;
+        userScoreText.textContent = `${userScore}`;
+        results.textContent = "Lose";
+      }
+    };
+
+    const endOfGame = (userChoices, round) => {
+      let winner = document.getElementById("winner-value-text");
+      if (userScore === cpuScore) {
+        winner.textContent = "It's a Tie!";
+      } else if (userScore > cpuScore) {
+        winner.textContent = "You Win the Game!";
+      } else {
+        winner.textContent = "You Lose!";
+      }
+    };
+  };
+  playGame();
 }
-game()
-
-
-
-
-
-
-
-
-
-
-
+game();
 
 //function setupGame() {
 //    createButtons()
@@ -240,12 +247,12 @@ game()
 //    let winner = document.getElementById("winner-value-text");
 //    let selection = document.getElementById("selection-text");
 //    if (cpuChoice === "rock") {
-//        selection.textContent = `It's a Draw! Computer Picked ${cpuChoice}` 
-//               
+//        selection.textContent = `It's a Draw! Computer Picked ${cpuChoice}`
+//
 //    } else if (cpuChoice === "paper") {
 //        selection.textContent = `You Lose! Computer Picked ${cpuChoice}`
 //    } else if (cpuChoice === "scissors"){
-//        selection.textContent = `You Win! Computer Picked ${cpuChoice}`        
+//        selection.textContent = `You Win! Computer Picked ${cpuChoice}`
 //    }
 //}
 //
